fbf98b5077e80fcd0624e9b93acbaf938a4b57ef
Marioditrani <marioditrani2003@gmail.com>
2023-09-19 23:48:43 +0200

    js

---
+++ script.js
@@ -0,0 +1,14 @@
+let Input = document.getElementById("input");
+console.log(Input);
+
+let Button = document.getElementById("enter")
+console.log(Button)
+
+
+
+
+Button.addEventListener("click", function() {
+    console.log(Input.value)
+   
+});
+
